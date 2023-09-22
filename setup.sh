59552a6a6a2a317167d4e66866b5541e789abf2b
octop162 <octop162@icloud.com>
2023-09-23 02:21:17 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ mkdir -p ~/.vim
 
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
-ln -sf ~/.dotfiles/.zshlocal  ~/.zshrc
+ln -sf ~/.dotfiles/.zshlocal  ~/.zshlocal
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
