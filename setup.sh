291bb328be3a6153d6110aafe3a7ce5348a2dc1f
octop162 <46759904+octop162@users.noreply.github.com>
2023-10-07 22:02:35 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/.vim
 
-ln -sf ~/.dotfiles/vimrc ~/.vimrc
-ln -sf ~/.dotfiles/zshrc ~/.zshrc
-ln -sf ~/.dotfiles/zshlocal  ~/.zshlocal
-ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/zshlocal  ~/.zshlocal
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
